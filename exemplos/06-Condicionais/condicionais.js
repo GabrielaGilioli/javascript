27d5301a85c17ae7c1bb3693872b207efe1680d9
GabrielaGilioli <gabrielagilioli.senai@gmail.com>
2023-09-28 10:00:33 -0300

    exemplos
    exemplos

--- exemplos/06-Condicionais/condicionais.js
+++ exemplos/06-Condicionais/condicionais.js
@@ -4,16 +4,21 @@ document.write('<h2 style="color: blue;">Condicional encadeada</h2><br>');
 
 /* if/else, else if */
 let nome = "Ignacio";
-let idade = 41;
 
 // Verificar idade da pessoa
 
 
 // Condicional encadeada
 let mensagem;
+let idade = 41;
 
 
-// 1ª Digitação (Aqui)
+
+if (idade > 18) {
+    mensagem = ("Você é maior de idade!");
+} else {
+    mensagem = ("Você é menor de idade!");
+}
 
 
 console.log(mensagem);
@@ -26,7 +31,18 @@ document.write('<h2 style="color: blue;">Condicional aninhada</h2><br>');
 
 
 
-// 2ª Digitação (Aqui)
+if(idade >=60) {
+    mensagem = "É idoso.";
+} else if (idade >= 18) {
+    mensagem = "É adulto, mas não é idoso.";
+} else {
+    //&& (Operador E)
+    if (idade >= 12 && idade <18) {
+        mensagem = "É adolescente.";
+    } else {
+        mensagem = "É baby.";
+    }
+}
 
 
 document.write(`${nome}, ${mensagem} <br>`); // Mostra a tela (Navegador)
@@ -42,7 +58,16 @@ var n1 = 5.7;
 var n2 = 9;
 
 
-// 3ª Digitação (Aqui)
+media = (n1 + n2) /2;
+//Modo tradicional
+// if (media >=7) {
+//     situacao = 'Aprovado!';
+// } else {
+//     situacao = 'Reprovado!';
+// }
+
+//Operador Ternário
+situacao = media >=7 ? 'Aprovado!' : 'Reprovado!';
 
 
 document.write(`Média: ${media}<br> `)
@@ -52,7 +77,7 @@ document.write("________________________________________________________________
 
 // condicional aninhada
 document.write('<h2 style="color: blue;">Switch Case</h2><br>');
-
+n
 
 /* switch/ case/ default/ break 
 Opçôes do Chat Bot:
@@ -65,8 +90,20 @@ let opcao = 4;
 let textoOpcao;
 
 
-
-// 4ª Digitação (Aqui)
+switch(opcao){
+    case 1:
+        textoOpcao = 'Legal, o que deseja saber?';
+        break;
+    case 2:
+        textoOpcao = 'Que pena, o que aconteceu?';
+        break;
+    case 3:
+        textoOpcao = 'Certo, qual a duvida?';
+        break;
+    case 4:
+        textoOpcao = 'Hum, não entendi... vou transferir';
+        break;
+}
 
 
 document.write(`Você escolheu a opção ${opcao} <br>`);
@@ -80,9 +117,10 @@ document.write('<h2 style="color: blue;">Operadores de comparação</h2><br>');
 // Operadores de comparação
 
 
-// 5ª Digitação (Aqui)
+let a  = 10;
+let b = '10';
 
 
 // == se é igual/ === estritamente igual (até tipo de dados)
-document.write(a == b); // True ou false
+document.write(a === b); // True ou false
 
