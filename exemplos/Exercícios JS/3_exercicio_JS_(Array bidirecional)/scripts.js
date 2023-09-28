5a96fa2b897832408baf00749d093b44e6a4134e
GabrielaGilioli <gabrielagilioli.senai@gmail.com>
2023-09-28 14:54:50 -0300

    exercicio
    exercicio

--- exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
+++ exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
@@ -4,3 +4,11 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
 
+document.write('<h2 style="color: purple;">Melhores professores do SENAI</h2><br>');
+
+let melhoresprofessores = [
+    ['Ignácio', 'Valmir', 'Bárbara', 'Nadja', 'Raul','Denis'],
+];
+
+document.write(`Melhores professores do SENAI: <strong>${melhoresprofessores[0]}</strong>`)
+
--- exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
+++ exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
@@ -4,5 +4,16 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
 
+let topModels = [
+    "Gisele Bundchen", "Naomi Campbell", "Cara Delavigne", 
+];
+
+//Cache do tamanho do array
+let tamanho = topModels.length;
+
+for (let i = 0; i < tamanho; i++) {
+    document.write(`<strong>${topModels[i]}</strong><br>`);
+}
+
 
 
